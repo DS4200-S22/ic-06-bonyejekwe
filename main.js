e99f68fef364c3bc9bd02f71b4670cbb4ce336bc
Brandon Onyejekwe <brandononyejekwe@Brandons-MacBook-Pro.local>
2022-02-10 13:25:21 -0500

    Activity

--- main.js
+++ main.js
@@ -9,6 +9,15 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
+
+function returnBiggerRand() {
+    let myRand = Math.random() * 100;
+    return myRand;
+}
+
 function button1Clicked() {
     console.log("Button 1 was clicked"); 
+    //new_num = returnBiggerRand();
+    //let randomNum = document.getElementById("random-num");
+    //randomNum.innerHTML = new_num;
 }
